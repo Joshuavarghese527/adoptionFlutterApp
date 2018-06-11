7e27761982efb01d36a306109e0770a92cb5894a
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-06-11 19:28:10 -0400

    UI for detail page + Tab Navigation + Details

--- lib/ui/cat_details/details_page.dart
+++ lib/ui/cat_details/details_page.dart
@@ -1,6 +1,10 @@
 import 'package:adoptionapp/models/cat.dart';
+import 'package:adoptionapp/ui/cat_details/details_body.dart';
+import 'package:adoptionapp/ui/cat_details/footer/details_footer.dart';
+import 'package:adoptionapp/ui/cat_details/header/details_header.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+
 
 class CatDetailsPage extends StatefulWidget {
   final Cat cat;
@@ -18,14 +22,33 @@ class CatDetailsPage extends StatefulWidget {
 class _CatDetailsPageState extends State<CatDetailsPage> {
   @override
   Widget build(BuildContext context) {
+    var linearGradient = new BoxDecoration(
+      gradient: new LinearGradient(
+        begin: FractionalOffset.centerRight,
+        end: FractionalOffset.bottomLeft,
+        colors: [
+          Colors.blue,
+          Colors.blue,
+        ],
+      ),
+    );
+
     return new Scaffold(
       body: new SingleChildScrollView(
         child: new Container(
-          //TODO Decoration
+          decoration: linearGradient,
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              //TODO Page Children
+              new CatDetailHeader(
+                widget.cat,
+                avatarTag: widget.avatarTag,
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: new CatDetailBody(widget.cat),
+              ),
+              new CatShowcase(widget.cat),
             ],
           ),
         ),
--- lib/ui/cat_details/footer/details_footer.dart
+++ lib/ui/cat_details/footer/details_footer.dart
@@ -1,4 +1,7 @@
 import 'package:adoptionapp/models/cat.dart';
+import 'package:adoptionapp/ui/cat_details/footer/showcase_cattributes.dart';
+import 'package:adoptionapp/ui/cat_details/footer/showcase_details.dart';
+import 'package:adoptionapp/ui/cat_details/footer/showcase_pictures.dart';
 import 'package:flutter/material.dart';
 
 class CatShowcase extends StatefulWidget {
@@ -12,9 +15,49 @@ class CatShowcase extends StatefulWidget {
 
 class _CatShowcaseState extends State<CatShowcase>
     with TickerProviderStateMixin {
+  List<Tab> _tabs;
+  List<Widget> _pages;
+  TabController _controller;
+
+  @override
+  initState() {
+    super.initState();
+    _tabs = [
+      new Tab(text: 'Pictures'),
+      new Tab(text: 'Details'),
+      new Tab(text: 'Cattributes'),
+    ];
+    _pages = [
+      new PicturesShowcase(widget.cat),
+      new DetailsShowcase(widget.cat),
+      new CattributesShowcase(widget.cat),
+    ];
+    _controller = new TabController(
+      length: _tabs.length,
+      vsync: this,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
+    return new Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: new Column(
+        children: [
+          new TabBar(
+            controller: _controller,
+            tabs: _tabs,
+            indicatorColor: Colors.white,
+          ),
+          new SizedBox.fromSize(
+            size: const Size.fromHeight(300.0),
+            child: new TabBarView(
+              controller: _controller,
+              children: _pages,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
