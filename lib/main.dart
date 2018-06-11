4dcd0b231599e362b5da987a06c710202d6d9ece
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-06-11 12:47:29 -0400

    Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:adoptionapp/ui/cat_list.dart';
+import 'package:flutter/material.dart';
+
+
+void main() async {
+  runApp(new AdoptionBoxApp());
+}
+
+class AdoptionBoxApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.pinkAccent,
+        ),
+      home: new CatList(),
+      );
+    }
+  }
+
