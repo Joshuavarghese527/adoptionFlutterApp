b0d368f28a42f19077ae452afbe882e82f9537b0
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-06-11 18:19:59 -0400

    Class Structure

---
+++ lib/ui/cat_details/details_page.dart
@@ -0,0 +1,14 @@
+import 'package:adoptionapp/models/cat.dart';
+import 'package:flutter/material.dart';
+import 'pacakage:meta/meta.dart';
+
+class CatDetailsPage extends StatefulWidget {
+  final Cat cat;
+  final Object avatarTag;
+}
+
+CatDetailsPage {
+  this.cat, {
+    @required
+  }
+}
